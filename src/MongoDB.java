b4ae63334bd782fbdb5589ec279f0423dd7e217b
VinMendes <Vinicius.mc6@puccampinas.edu.br>
2024-10-09 08:32:09 -0300

    Servido (sem thread)

---
+++ src/MongoDB.java
@@ -0,0 +1,40 @@
+import com.mongodb.*;
+import com.mongodb.client.*;
+import org.bson.Document;
+
+public class MongoDB {
+
+    // **Importante**: Evite expor credenciais diretamente no código.
+    // Considere utilizar variáveis de ambiente ou arquivos de configuração seguros.
+    final static String connectionString = "mongodb+srv://viniciusmc6:051007@fidelizapi4.i1nd1.mongodb.net/?retryWrites=true&w=majority&appName=FidelizaPI4";
+
+    private static final ServerApi serverApi = ServerApi.builder()
+            .version(ServerApiVersion.V1)
+            .build();
+
+    private static final MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(new ConnectionString(connectionString))
+            .serverApi(serverApi)
+            .build();
+
+
+    public static void insertDocument(String jsonDocument) throws Exception {
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            // Conectando ao banco de dados 'empresa'
+            MongoDatabase database = mongoClient.getDatabase("mensagem");
+
+            // Acessando a coleção 'empresas'
+            MongoCollection<Document> collection = database.getCollection("mensagens");
+
+            // Convertendo o JSON para um documento BSON
+            Document document = Document.parse(jsonDocument);
+
+            // Inserindo o documento na coleção
+            collection.insertOne(document);
+        } catch (MongoException e) {
+            e.printStackTrace();
+            throw new Exception("Erro ao conectar ou inserir o documento no MongoDB.", e);
+        }
+    }
+
+}
---
+++ src/Servidor.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+        MongoDB mongo;
+
+        try {
+            server = new ServerSocket(12345);
+            System.out.println("Servidor iniciado na porta 12345.");
+
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("Novo cliente conectado: " + socket.getInetAddress());
+
+            }
+
+        } catch (Exception err) {
+            System.err.println(err.getMessage());
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
